350fff3d5ba581b19b4cc54fbdb53a2027670f0a
caio <caio.secco@hotmail.com>
2026-01-21 17:20:38 -0300

    adding user data json

--- cypress/e2e/Loginspec.cy.js
+++ cypress/e2e/Loginspec.cy.js
@@ -1,9 +1,21 @@
+import userData from '../fixtures/userData.json'
+
 describe('Página de Login', () => {
+
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    dashboardGrid: ".orangehrm-dashboard-grid"
+  }
+
+
   it('Deve acessar a página de login e realizar o cadastro com sucesso', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Admin')
-    cy.get('[name="password"]').type('admin123')
-    cy.get('[type="submit"]').click()
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardGrid) //Valida se eralmente está no dashboard
   })
 })
