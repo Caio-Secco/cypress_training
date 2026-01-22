b151c35881847b2444bf658cb2e41e3eb13100b7
caio <caio.secco@hotmail.com>
2026-01-21 21:23:43 -0300

    User Update

--- cypress/e2e/Loginspec.cy.js
+++ cypress/e2e/Loginspec.cy.js
@@ -6,7 +6,14 @@ describe('Página de Login', () => {
     usernameField: "[name='username']",
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
-    dashboardGrid: ".orangehrm-dashboard-grid"
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    middleNameField: "[name='middleName']",
+    lastNameField: "[name='lastName']",
+    genericNameField: ".oxd-input--active",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']"
   }
 
   it('Deve acessar a página de login e realizar o cadastro com sucesso', () => {
@@ -16,5 +23,16 @@ describe('Página de Login', () => {
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid) //Valida se eralmente está no dashboard
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('Caio')
+    cy.get(selectorsList.middleNameField).clear().type('teste')
+    cy.get(selectorsList.lastNameField).clear().type('1234')
+    cy.get(selectorsList.genericNameField).eq(3).clear().type('caioba')
+    cy.get(selectorsList.genericNameField).eq(4).clear().type('caiobatwo')
+    cy.get(selectorsList.genericNameField).eq(5).clear().type('caiobatree')
+    cy.get(selectorsList.genericNameField).eq(6).clear().type('2002-04-30')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('.oxd-text--toast-message')
   })
 })
