9a42d8d805f656ed5b1ba0d1eb2f669118d09970
caio <caio.secco@hotmail.com>
2026-01-27 22:04:59 -0300

    code update

--- cypress/e2e/userspec.cy.js
+++ cypress/e2e/userspec.cy.js
@@ -1,4 +1,4 @@
-import userData from '../fixtures/userData.json'
+/*import userData from '../fixtures/userData.json'
 import LoginPage from '../pages/loginPage'
 import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
@@ -28,6 +28,59 @@ describe('Testes Orange', () => {
 
     pageMyInfo.contactDetailsAdress(chance.street(), chance.street(), chance.city(), chance.state({ full: true }), chance.postal())
     pageMyInfo.contactDetailsContacts(chance.phone(), chance.phone(), chance.phone(), chance.email(), chance.email())
+    pageMyInfo.saveForm()
+
+  })
+
+})
+*/
+
+import userData from '../fixtures/userData.json'
+import LoginPage from '../pages/loginPage'
+import DashboardPage from '../pages/dashboardPage'
+import MenuPage from '../pages/menuPage'
+import MyInfopage from '../pages/myinfoPage'
+
+const Chance = require('chance')
+
+const chance = new Chance()
+const pageLogin = new LoginPage()
+const pageDashboard = new DashboardPage()
+const pageMenu = new MenuPage()
+const pageMyInfo = new MyInfopage()
+
+describe('Testes Orange', () => {
+
+  beforeEach(() => {
+    pageLogin.accessLoginPage()
+    pageLogin.loginWhithUser(
+      userData.userSuccess.username,
+      userData.userSuccess.password
+    )
+    pageDashboard.checkDashboardPage()
+    pageMenu.ButtonMyInfo()
+  })
+
+  it('Deve fazer o login corretamente ser redirecionado para a página de MyInfo, incluir os dados corretamente e salvar.', () => {
+
+    pageMyInfo.personalDetails(chance.first(), chance.last(), chance.string())
+    pageMyInfo.employeedDetails('employeed', 'orherTest', '1324', '2026-01-26')
+    pageMyInfo.statusDetails()
+
+    pageMyInfo.contactDetailsAdress(
+      chance.street(),
+      chance.street(),
+      chance.city(),
+      chance.state({ full: true }),
+      chance.postal()
+    )
+    pageMyInfo.contactDetailsContacts(
+      chance.phone(),
+      chance.phone(),
+      chance.phone(),
+      chance.email(),
+      chance.email()
+    )
     pageMyInfo.saveForm()
 
   })
